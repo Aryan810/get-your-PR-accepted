ac3d0f56969a30b44c6ce0928afde6822b6c8029
Aryan810 <67587135+Aryan810@users.noreply.github.com>
2021-10-03 12:38:54 +0530

    Update CrossSearch.py

--- Searching/Python/CrossSearch.py
+++ Searching/Python/CrossSearch.py
@@ -2,19 +2,25 @@ from threading import Thread
 import time
 
 
-class SearchAlgorithm1:
+class CrossSearch:
 
     def __init__(self, list_data: list, element):
+        #  Initializing variables asper the arguments.
         self.element = element
         self.list = list_data
         self.searching = True
         self.index = None
-
+    
+    # Function to start searching, it starts a thread for forward function
+    # and then starts the reverse function in main thread.
+    # It also returns the result i.e the index of the element
+    # which will be found by the reverse or forward function
     def search(self):
         Thread(self.forward())
         self.reverse()
         return self.index
-
+ 
+    # This function checks the element from end.
     def reverse(self):
         len_all = len(self.list)
         for i in range(len(self.list) - 1):
@@ -29,6 +35,7 @@ class SearchAlgorithm1:
             except Exception:
                 pass
 
+    # This function checks the element from start.
     def forward(self):
         for i in range(len(self.list) - 1):
             try:
@@ -45,5 +52,5 @@ class SearchAlgorithm1:
 list_i = [i for i in range(800, 9800)]
 print("Searching by my algorithm...")
 
-index_element = SearchAlgorithm1(list_i, 8999).search()
+index_element = CrossSearch(list_i, 8999).search()
 print(index_element)
